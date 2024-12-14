6eb6a7bb9fb62c80ab88547b7663167697ac8013
susTuna <hidasetsuna@gmail.com>
2024-12-14 22:13:49 +0700

    feat: disabled image API, enabled audio API

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
+# from feature.album_picture_finder.api_image_retrieval import image_api
+from feature.music_information_retrieval.api_music_retrieval import music_api
+
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+# Register the Blueprint
+app.register_blueprint(music_api, url_prefix='/')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
