93daf09333637ab7586af73561e508f63f5bf800
susTuna <hidasetsuna@gmail.com>
2024-12-14 22:10:24 +0700

    feat: integrated backend audio fetching API to frontend

--- src/frontend/src/app/audiosearch/page.tsx
+++ src/frontend/src/app/audiosearch/page.tsx
@@ -1,25 +1,34 @@
 import {AudioCard} from "@/components/audiocard";
 import SideBar from "@/components/sidebar";
+import UploadMIDI from "@/components/audiosearch";
+
+// export default function Home() {
+//   const anjay = {
+//     name: "Anjay",
+//     artist: "Krisyanto",
+//     album: "Anjay",
+//     duration: "3:45",
+//     image: "/ajay.jpg",
+//   }
+//   return ( 
+//     <main className="bg-gradient-to-b from-[#e0d8ff] from-10% to-blue to-90% flex min-h-screen flex-col items-center">
+//       <SideBar />
+//       <div className="w-full flex flex-col items-center justify-center">
+//         <h1 className="text-[#46404F] text-center w-full text-6xl font-extrabold mb-6">
+//           Search by Audio
+//         </h1>
+//         <div className="gap-4 gap-y-6 mt-12">
+//           <AudioCard songs={anjay} />
+//         </div>
+//       </div>
+//     </main>
+//   );
+// }
 
 export default function Home() {
-  const anjay = {
-    name: "Anjay",
-    artist: "Krisyanto",
-    album: "Anjay",
-    duration: "3:45",
-    image: "/ajay.jpg",
-  }
-  return ( 
-    <main className="bg-gradient-to-b from-[#e0d8ff] from-10% to-blue to-90% flex min-h-screen flex-col items-center">
-      <SideBar />
-      <div className="w-full flex flex-col items-center justify-center">
-        <h1 className="text-[#46404F] text-center w-full text-6xl font-extrabold mb-6">
-          Search by Audio
-        </h1>
-        <div className="gap-4 gap-y-6 mt-12">
-          <AudioCard songs={anjay} />
-        </div>
-      </div>
-    </main>
+  return (
+    <div>
+      <UploadMIDI />
+    </div>
   );
 }
