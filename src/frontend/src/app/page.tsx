523eb34c41e2c33aa42ae42fce4fa28f337741b3
susTuna <hidasetsuna@gmail.com>
2024-12-15 14:39:15 +0700

    feat: changed the homepage layout

--- src/frontend/src/app/page.tsx
+++ src/frontend/src/app/page.tsx
@@ -2,12 +2,18 @@ import SideBar from "@/components/sidebar";
 
 export default function Home() {
   return ( 
-    <main className="bg-gradient-to-b from-[#e0d8ff] from-10% to-black to-90% flex min-h-screen flex-col items-center">
-      <SideBar />
-      <div className="w-full flex flex-col items-center justify-center">
-        <h1 className="text-[#46404F] text-center w-full text-6xl font-extrabold mb-6">
-          Home
+    <main className="flex min-h-screen bg-[#0b0e26]">
+      {/* Sidebar */}
+      <div className="w-64 bg-white">
+        <SideBar />
+      </div>
+      <div className="w-full flex-1 flex-col items-center justify-center">
+        <h1 className="text-white text-center p-6 font-sans text-6xl font-extrabold">
+          Home Aja
         </h1>
+        <p className="text-white text-center font-sans text-2xl font-semibold">
+          Udah Gitu Aja Enjoy :>
+        </p>
       </div>
     </main>
   );
